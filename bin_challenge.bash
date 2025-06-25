49fbf556f6dbaf51189c7a5d9784909dd60168c7
anas <anasshahzad1996>
2025-06-25 22:48:25 +0200

    Add bash file

---
+++ bin_challenge.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+PY_SCRIPTS=(
+  "door_handle.py"
+  "emergency_button.py"
+  "barcode_scanner.py"
+  "hmi.py"
+)
+
+source ~/ros2_ws/install/setup.bash
+
+for script in "${PY_SCRIPTS[@]}"
+do
+  echo "Starting $script ..."
+  ros2 run bin_picking_challenge "$script" &
+done
+
+echo "All scripts started in background."
+
+wait
